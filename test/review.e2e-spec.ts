0834f9d24be84d1048231c8e246a2356b1bfca82
victordruc <druc92@gmail.com>
2023-03-30 00:28:32 +0300

    Added validation pipe

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -41,6 +41,13 @@ describe('AppController (e2e)', () => {
       });
   });
 
+  it('/review/create (POST) - fail', () => {
+    return request(app.getHttpServer())
+      .post('/review/create')
+      .send({ ...testDto, rating: 0 })
+      .expect(400)
+  });
+
   it('/review/byProduct/:productId (GET) - success', (done) => {
     request(app.getHttpServer())
       .get('/review/byProduct/' + productId)
@@ -62,7 +69,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/review/:id (DELETE) - success', () => {
-   return request(app.getHttpServer())
+    return request(app.getHttpServer())
       .delete('/review/' + createdId)
       .expect(200);
   });
