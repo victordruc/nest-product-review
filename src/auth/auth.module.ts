b8b6154f9515c4e37b56a1a42bf8c6a80a552ffd
victordruc <druc92@gmail.com>
2023-03-30 22:10:23 +0300

    Added login and register for User

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 
 import { AuthController } from './auth.controller';
-import { AuthModel } from './auth.model';
+import { UserModel } from './user.model';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from 'src/configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
   imports: [
     TypegooseModule.forFeature([
-      { typegooseClass: AuthModel, schemaOptions: { collection: 'Auth' } },
+      { typegooseClass: UserModel, schemaOptions: { collection: 'User' } },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig,
+    }),
   ],
+  providers: [AuthService],
 })
 export class AuthModule {}
